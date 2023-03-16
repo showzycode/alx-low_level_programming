35638c0e064f82e360c358b11f13d1eaeb94f362
showzycode <shalomezeliel112@gmail.com>
2023-03-16 15:42:02 -0700

    my commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,12 @@
-#inclued <stdio.h>
+#include <stdio.>
 /**
  * main - Entry point
  *
- * Return: Always 0 (success)
+ * Return 0 (Success)
  */
 
 int main(void)
-{	
-    puts("\"Programming is like building a multilingual puzzle");
-	       
-       	return (0);
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
