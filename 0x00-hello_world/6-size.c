be0a4f9fd14e83aaf3b1e281b99242e10a24b9a6
showzycode <shalomezeliel112@gmail.com>
2023-03-16 13:51:44 -0700

    my commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,15 +2,15 @@
  /**
  *main -Entry point
  *Return: Always 0 (Success)
+ *
  */
 int main(void)
 {
-        printf("int size: %lu bytes(s)\n", sizeof(int));
-        printf("char size: %lu bytes(s)\n", sizeof(char));
-        printf("short size: %lu bytes(s)\n", sizeof(short));
-        printf("long size: %lu bytes(s)\n", sizeof(long));
-        printf("float size: %lu bytes(s)\n", sizeof(float));
-        printf("double size: %lu bytes(s)\n", sizeof(double));
-        printf("long double size: %lu bytes(s)\n", sizeof(long double));
+        printf("int size: %zu bytes(s)\n", sizeof(int));
+        printf("char size: %zu bytes(s)\n", sizeof(char));
+        printf("long int size: %zu bytes(s)\n", sizeof(long int));
+        printf("float size: %zu bytes(s)\n", sizeof(float));
+        printf("long long int size: %zu bytes(s)\n", sizeof(long long int));
+	
 	return (0);
 }
