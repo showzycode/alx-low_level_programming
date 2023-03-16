d5ac92e70d2201e38080ad17f8f1ad5a0621a4d0
showzycode <shalomezeliel112@gmail.com>
2023-03-16 16:07:43 -0700

    my commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,15 +1,21 @@
 #include <stdio.h>
  /**
- *main -Entry point
- *Return: Always 0 (Success)
+ * main - A program that print the size of various computer types
+ * Return: 0 (Success)
  *
  */
+
 int main(void)
 {
-printf("int size: %zu bytes(s)\n", sizeof(int));
-printf("char size: %zu bytes(s)\n", sizeof(char));
-printf("long int size: %zu bytes(s)\n", sizeof(long int));
-printf("float size: %zu bytes(s)\n", sizeof(float));
-printf("long long int size: %zu bytes(s)\n", sizeof(long long int));
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
+printf("int size: %lu bytes(s)\n", (unsigned int)sizeof(a));
+printf("char size: %lu bytes(s)\n", (unsigned char)sizeof(b));
+printf("long int size: %lu bytes(s)\n", (unsigned long int)sizeof(c));
+printf("float size: %lu bytes(s)\n", (unsigned float)sizeof(d));
+printf("long long int size: %lu bytes(s)\n", (unsigned long long int)sizeof(f));
 return (0);
 }
