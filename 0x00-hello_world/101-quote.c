4b25e31b9451fe26711a6485c91db9840acf59a2
showzycode <shalomezeliel112@gmail.com>
2023-03-16 16:37:06 -0700

    my commit -m

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,5 @@
-#include<stdio.h>
-#include<unistd.h>
+#include <stdio.h>
+#include <unistd.h>
 /**
  * main - Entry point
  *
@@ -8,5 +8,5 @@
 int main(vioid)
 {
       write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-return (1);
+      return (1);
 }
