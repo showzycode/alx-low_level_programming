113c1ca06012e6704d04d765946dc659e6c0e4d9
Shalom Ekekiel <107555717+showzycode@users.noreply.github.com>
2023-03-19 10:52:01 -0700

    Update 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -10,19 +10,19 @@
  * Return always 0
  */
  {
-int n;
+         int n;
 
 	 if (n > 0)
-{
+
          printf("%d is a positive\n", n); 
-}
-{
+
+
          else if (n == 0)}
         
 	 printf("%d is a zero\n", n);
-}
+
          else
-{
+
          printf("%d is a nagative number\n", n);
          return (0);
 }
