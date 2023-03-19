9c78ceeb4a308cc900205ba4bfff9ef2c067b2a0
showzycode <shalomezeliel112@gmail.com>
2023-03-19 11:39:09 -0700

    my commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,28 +1,20 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdio.h>
-/* more headers gose there */
-
-/* betty style doc for function main goes there */
 /**
- * main - Entry point
- * 
- * Return always 0
+ *main - Entry point
+ *Return always 0
  */
- {
-int n;
 
-	 if (n > 0)
+int main(void)
 {
-         printf("%d is a positive\n", n); 
-}
-{
-         else if (n == 0)}
-        
-	 printf("%d is a zero\n", n);
-}
-         else
-{
-         printf("%d is a nagative number\n", n);
-         return (0);
+if (n > 0)
+printf("%d is a positive\n", n);
+
+else if (n == 0)}
+printf("%d is a zero\n", n);
+else
+
+printf("%d is a nagative number\n", n);
+return (0);
 }
