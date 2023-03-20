905ac1d1290ef875b0306aff07c22dfed6659209
showzycode <shalomezeliel112@gmail.com>
2023-03-19 18:35:05 -0700

    my commit

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,24 +1,34 @@
 #include <stdio.h>
+#include <stdio.h>
+
 /**
  * main - Numbers from 0 to 99.
  *
  * REturn: always return 0.
  */
+
 int main(void)
 {
 	int i;
 	int j;
 
-	for (i =0; i < 10; i++)
+	for (i = 0; i < 100; i++)
 	{
-		for (j =0; < 10; j++)
+		for (j = i; j < 100; j++)
 		{
-			putchar(i + '0');
-			putchar(i + '0');
-			putchar(j + '0');
-			putchar(j + '0');
-			putchar(',');
-			putchar(' ');
+			if (j != i)
+			{
+				putchar(i / 10 % 10 + '0');
+				putchar(i % 10 + 48);
+				putchar(' ');
+				putchar((j / 10) && 10 + '0');
+				putchar(j % 10 + '0');
+				if (i != 98 || j != 99)
+				{
+					putchar(',');
+					putchar(',');
+				}
+			}
 
 		}
 	}
