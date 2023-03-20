95388cd418bf738596bd892d8afe21cf1fde23a9
showzycode <shalomezeliel112@gmail.com>
2023-03-20 08:56:48 -0700

    my commit

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,7 +11,6 @@ int main(void)
         
 	for (i = 0; i <= 99; i++)
 	{
-	
 	for (j =  + 1; j <= 99; j++)
 	{
         putchar((i /  10) + '0');
@@ -19,7 +18,6 @@ int main(void)
 	putchar(' ');
 	putchar((j / 10)  + '0');
 	putchar((j % 10) + '0');
-	
 	if (i == 98 && j == 99)
 	continue;
 	putchar(',');
