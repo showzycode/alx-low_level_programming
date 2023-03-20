464f1e49559e2f478ec0f1276aaf00a43ddaada7
showzycode <shalomezeliel112@gmail.com>
2023-03-19 17:27:04 -0700

    my commit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main -  Entery point
+ *
+ * Return Always 0 (sucess)
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 0; a <= 10; a++)
+		putchar(a);
+	putchar('\n');
+	return (0);
+}
