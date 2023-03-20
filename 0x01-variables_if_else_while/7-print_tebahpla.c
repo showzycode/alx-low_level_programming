7d74483a0947f8492f36eabcdf6230aa6f62dbe5
showzycode <shalomezeliel112@gmail.com>
2023-03-20 09:34:30 -0700

    my commit

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entery point
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char a;
+
+	for (a = 'z'; a >= 'a'; a--)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
