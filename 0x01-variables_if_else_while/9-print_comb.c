3748b475af52f2f024c035979988ee554e9bbee8
showzycode <shalomezeliel112@gmail.com>
2023-03-20 10:33:35 -0700

    this is a program that reads all combinations of single-digit number

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -5,15 +5,15 @@
  * Return: always 0
  */
 int main(void)
-{	
+{
 	int n;
 
 	for (n = 48; n <= 57; n++)
-	{	
+	{
 		putchar(n);
 		putchar(',');
-		putchar(',');
-        }
+		putchar(' ');
+	}
 	putchar('\n');
 	return (0);
 }
