21490977a87125276605637978f9a0c85c26d433
showzycode <shalomezeliel112@gmail.com>
2023-03-20 10:13:51 -0700

    my commit

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
 /**
- *main - Entry point 
+ *main - Entry point
  *Discription: 'print numbers of base 16'
  *Return: 0
  */
 int main(void)
-
+{
 	int n;
-	int m;
+	char m;
 
-	for (n =48; n<= 57; n++)
-        {
+	for (n = 48; n < 58; n++)
+	{
 		putchar(n);
 	}
-        for (n = 97; m <= 102; m++){
+	for (m = 'a'; m <= 'f'; m++)
+	{
+		putchar(m);
 	}
-			{
-	
-        putchar('\n')
+	putchar('\n');
 	return (0);
-}	
+}
