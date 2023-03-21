775be9e1cf05c6ef0a0350043d4dd22bf4c7a7d1
showzycode <shalomezeliel112@gmail.com>
2023-03-21 15:51:48 -0700

    my commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - print putchar
+ * Return: Always 0
+ */
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	return ('\n');
+	return (0);
+}
