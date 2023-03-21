7962a33678f25c1b74b92ef1cef97576bc14d4c2
showzycode <shalomezeliel112@gmail.com>
2023-03-20 17:03:37 -0700

    prints all possible different combinations of three digits.

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -16,6 +16,7 @@ int main(void)
 		for (j = i; j < 10; j++)
 			for (k = 2; k < 10; k++)
 			{
+				if (i < j && j < k)
 				putchar(i + '0');
 				putchar(j + '0');
 				putchar(k + '0');
--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,29 +7,19 @@
 
 int main(void)
 {
-	int i;
-	int j;
-
-        for (i = 0; i <= 99; i++)
-
+	for (i = 0; i <= 9; i++)
 	{
-			for (j = + 1; j <= 99; j++)
 		{
+			putchar(i + '0');
+			putchar(j + '0);
+			if (i == 8 && j == 9)
+				putchar('\n');
+			else
 			{
-				putchar((i /  10) + '0');
-				putchar((i % 10) + '0');
-				putchar(' ');
-				putchar((j / 10) + '0');
-				putchar((j % 10) + '0');
+				putchar(',');
+				putchar(',');
 			}
 		}
 	}
-		if (i == 98 && j == 27)
-		continue;
-		putchar(',');
-		putchar(' ');
-		{
-		}
-		putchar('\n');
-		return (0);
+	return (0);
 }
