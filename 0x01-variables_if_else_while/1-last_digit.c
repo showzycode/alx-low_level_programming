0c365b79f073b2b913d4a349f499bc69de8b7fbc
showzycode <shalomezeliel112@gmail.com>
2023-03-20 19:17:30 -0700

    print the last digit of the number stored in the variable

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -8,9 +8,9 @@
   */
 int main(void)
 {
-        int n;
-        int ld;
-        srand(time(0));
+	int n;
+	int ld;
+   	srand(time(0));
         n = rand() - RAND_MAX / 2;
         ld = n % 10;
         if (ld > 5)
