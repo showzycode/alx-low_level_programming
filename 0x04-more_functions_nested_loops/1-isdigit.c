0feb68347e5db4bb65116e62edc4ffa173d18425
showzycode <shalomezeliel112@gmail.com>
2023-03-23 15:05:16 -0700

    function that prints 10 times the numbers

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,4 +1,4 @@
-#unclude "main.h"
+#include "main.h"
 /**
 * _isdigit - check if numbers are 0 - 9
 * @c: char to be checked
@@ -7,7 +7,7 @@
 */
 int _isdigit(int c)
 {
-	if (c <= '0' && c >= '9')
+	if (c >= '0' && c <= '9')
 		return (1);
 	else
 		return (0);
