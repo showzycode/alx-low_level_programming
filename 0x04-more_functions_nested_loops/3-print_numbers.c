5da23f9cce99920274a27707a80285637e7576ff
showzycode <shalomezeliel112@gmail.com>
2023-03-23 14:14:41 -0700

    my commit

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * print_numbers - Print numbers between 0 to 9 incl.
+ * print_numbers - Print numbers between 0 to 9 .
  *
  * Return: Void.
  */
@@ -12,7 +12,7 @@ void print_numbers(void)
 
 	for (i = 0; i < 10; i++)
 	{
-		_putchar(i);
+		_putchar(ii);
 	}
 	_putchar('\n');
 }
