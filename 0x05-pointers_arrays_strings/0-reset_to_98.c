94827cc996b77bbfbf4494622d7a6795d4183698
showzycode <shalomezeliel112@gmail.com>
2023-03-28 04:57:09 -0700

    function that takes a pointer to an int

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * reset_to_98 - update the value of n to 98
  * @n: input
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,5 +1,5 @@
-#ifndef "MAIN_H"
-#define "MAIN_H"
+#ifndef MAIN_H
+#define MAIN_H
 #include <stdio.h>
 
 int _putchar(char c);
@@ -15,4 +15,4 @@ void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 
-#endif 
+#endif
