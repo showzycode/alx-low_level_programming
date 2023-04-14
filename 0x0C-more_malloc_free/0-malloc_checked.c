01dc0df0f0ec24570f3e1db1e3c5b6f9d08c6096
showzycode <shalomezeliel112@gmail.com>
2023-04-14 02:46:44 -0700

    my commit

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,19 +1,20 @@
+#include <stdlib.h>
 #include "main.h"
-#include "stdlib.h"
-/**
- *malloc_checked- Function that allocates memory
- *@b: unsigned interger
- *Exit: 98
- *Return: pointer to the allocated memory *Ptr
- */
 
+/**
+ * *malloc_checked - allocates memory using malloc
+ * @b: number of bytes to allocate
+ *
+ * Return: a pointer to the allocated memory
+ */
 void *malloc_checked(unsigned int b)
 {
-	void *ptr = malloc(b);
+	void *ptr;
 
-	if (ptr == 0)
-	{
+	ptr = malloc(b);
+
+	if (ptr == NULL)
 		exit(98);
-	}
+
 	return (ptr);
 }
